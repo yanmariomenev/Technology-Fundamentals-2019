b190004f619b5ca58f99f88a2279330b2453b5ca
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2019-04-07 20:54:20 +0300

    Added new exam solutions

---
+++ Technology Fundamentals 2019/Technology Fundamentals Final Exam - 16 December 2018/01.Concert/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Concert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bandPlayTime = new Dictionary<string, int>();
+            var bandLineUp = new Dictionary<string, List<string>>();
+            var startBand = string.Empty;
+            while (true)
+            {
+                var command = Console.ReadLine().Split("; ").ToList();
+
+                var addOrPlay = command[0];
+
+                if (addOrPlay == "start of concert")
+                {
+                    startBand = Console.ReadLine();
+                    break;
+                }
+                var bandName = command[1];
+                var memberList = command[2].Split(", ");
+                if (addOrPlay == "Play")
+                {
+                    var playTime = int.Parse(command[2]);
+                    if (!bandPlayTime.ContainsKey(bandName))
+                    {
+                        bandPlayTime.Add(bandName, playTime);
+                    }
+                    else
+                    {
+                        bandPlayTime[bandName] += playTime;
+                    }
+                }
+                else
+                {
+                    if (!bandLineUp.ContainsKey(bandName))
+                    {
+                        bandLineUp[bandName] = new List<string>();
+                        bandLineUp[bandName].AddRange(memberList);
+                        //for (int i = 2; i < command.Count; i++)
+                        //{
+                        //    bandLineUp[bandName].Add(command[i]);
+                        //}
+                    }
+                    else
+                    {
+                        for (int j = 0; j < memberList.Length; j++)
+                        {
+                            string member = memberList[j];
+                            if (!bandLineUp[bandName].Contains(member))
+                            {
+                                bandLineUp[bandName].Add(member);
+                            }
+                        }
+                    }
+                }
+            }
+            var totalTime = bandPlayTime.Sum(x => x.Value);
+            var sortBand = bandPlayTime.OrderByDescending(x => x.Value).ThenBy(x => x.Key).ToList();
+            Console.WriteLine($"Total time: {totalTime}");
+            foreach (var kvp in sortBand)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+            Console.WriteLine(startBand);
+            var bandMember = bandLineUp[startBand].ToList();
+
+            foreach (var member in bandMember)
+            {
+                Console.WriteLine($"=> {member}");
+            }
+
+        }
+    }
+}
---
+++ Technology Fundamentals 2019/Technology Fundamentals Final Exam - 16 December 2018/02.Song Encryption/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.Song_Encryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var artistPattern = @"^[A-Z][a-z]+[ |a-z| ']*$";
+            Regex artistRegex = new Regex(artistPattern);
+
+            var songPattern = @"^[A-Z]+[ |A-Z]*$";
+            var songRegex = new Regex(songPattern);
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                var splittedInput = input.Split(":");
+                var artist = splittedInput[0];
+                var song = splittedInput[1];
+                if (artistRegex.IsMatch(artist) && songRegex.IsMatch(song))
+                {
+                    var key = artist.Length;
+                    Decription(input, key);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input!");
+                }
+            }
+        }
+
+        private static void Decription(string input, int key)
+        {
+            string decryptedInput = string.Empty;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char characterCheck = input[i];
+                if (characterCheck == ' ')
+                {
+                    decryptedInput += characterCheck;
+                }
+                else if (characterCheck == ':')
+                {
+                    decryptedInput += '@';
+                }
+                else if (characterCheck == 39)
+                {
+                    decryptedInput += (char)39;
+                }
+                else
+                {
+                    if (characterCheck >= 65 && characterCheck <= 90)
+                    {
+                        if (characterCheck + key <= 90)
+                        {
+                            decryptedInput += (char)(characterCheck + key);
+                        }
+                        else
+                        {
+                            int resetChar = characterCheck + key;
+                            while (resetChar > 90)
+                            {
+                                int diffrence = resetChar - 90;
+                                resetChar = 64 + diffrence;
+                            }
+                            decryptedInput += (char)resetChar;
+                        }
+                    }
+                    else if (characterCheck >= 97 && characterCheck <= 122)
+                    {
+                        if (characterCheck + key <= 122)
+                        {
+                            decryptedInput += (char)(characterCheck + key);
+                        }
+                        else
+                        {
+                            int resetChar = characterCheck + key;
+                            while (resetChar > 122)
+                            {
+                                int diffrence = resetChar - 122;
+                                resetChar = 96 + diffrence;
+                            }
+                            decryptedInput += (char)resetChar;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine($"Successful encryption: {decryptedInput}");
+        }
+    }
+}
