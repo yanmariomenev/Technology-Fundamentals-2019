b0ac0a17610af6b20619e4d8f007fd3cf242701a
Yanmario Menev <49076727+yanmariomenev@users.noreply.github.com>
2019-04-08 19:10:08 +0300

    Added new exam with solutions

---
+++ Technology Fundamentals 2019/Technology Fundamentals Final Exam - 06 April 2019/01.Dictionary/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var defenisions = new SortedDictionary<string, List<string>>();
+
+            while (true)
+            {
+                var input = Console.ReadLine().Trim().Split("|").ToArray();
+                for (int i = 0; i < input.Length; i++)
+                {
+                    var splitted = input[i].Split(":").ToList();
+                    var word = splitted[0].Trim();
+                    if (word == "End")
+                    {
+                        foreach (var item in defenisions)
+                        {
+                            Console.WriteLine($"{item.Key}");
+                            var sort = item.Value.OrderByDescending(x => x.Length).ToList();
+                            for (int j = 0; j < sort.Count; j++)
+                            {
+                                Console.WriteLine($"-{sort[j]}");
+                            }
+
+
+                        }
+                        return;
+                    }
+                    if (word == "List")
+                    {
+                        foreach (var item in defenisions)
+                        {
+                            Console.Write($"{item.Key} ");
+                        }
+                        return;
+                    }
+                    if (splitted.Count > 1)
+                    {
+                        var defenision = splitted[1].Trim();
+                        if (!defenisions.ContainsKey(word))
+                        {
+                            defenisions[word] = new List<string>();
+                            defenisions[word].Add(defenision);
+                        }
+                        else
+                        {
+                            defenisions[word].Add(defenision);
+                        }
+                    }
+
+                }
+
+            }
+        }
+    }
+}
---
+++ Technology Fundamentals 2019/Technology Fundamentals Final Exam - 06 April 2019/02.Deciphering/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.Deciphering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var text = Console.ReadLine().ToList();
+            var addition = Console.ReadLine().Split().ToArray();
+            var decripted = new StringBuilder();
+            var get = addition[0];
+            var set = addition[1];
+            var key = -3;
+            for (int i = 0; i < text.Count; i++)
+            {
+                var charackterCheck = text[i];
+                if (charackterCheck == 'a' || charackterCheck == 'b'|| charackterCheck == 'c' ) 
+                {
+                    Console.WriteLine("This is not the book you are looking for.");
+                    return;
+                }
+                if (charackterCheck == '#')
+                {
+                    decripted.Append(' ');
+                }
+                else
+                {
+                    decripted.Append((char)(charackterCheck + key));
+                    
+                }
+                decripted.Replace(get, set);
+            }
+            Console.WriteLine(decripted);
+            
+        }
+    }
+}
